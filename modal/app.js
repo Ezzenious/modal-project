23633f96a9208a0729c4ba02163ac04dd2f55df3
Ben Wagner <bmwbenswagner@gmail.com>
2022-07-28 21:51:56 -0500

    Add code to close modal when container is clicked

--- modal/app.js
+++ modal/app.js
@@ -1,11 +1,17 @@
-let openBtn = document.getElementById('open-btn');
-let modalContainer = document.getElementById('modal-container');
-let closeBtn = document.getElementById('close-btn')
+let openBtn = document.getElementById("open-btn");
+let modalContainer = document.getElementById("modal-container");
+let closeBtn = document.getElementById("close-btn");
 
-openBtn.addEventListener('click', () => {
-    modalContainer.style.display = 'block';
+openBtn.addEventListener("click", () => {
+  modalContainer.style.display = "block";
 });
 
-closeBtn.addEventListener('click', () => {
-    modalContainer.style.display = 'none';
+closeBtn.addEventListener("click", () => {
+  modalContainer.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modalContainer) {
+    modalContainer.style.display = "none";
+  }
 });
